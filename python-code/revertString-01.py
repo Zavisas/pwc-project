da522a5f77348ff30dbf1e38fda06253166e29b0
Joao Zavisas <joaoterassi01@gmail.com>
2023-07-07 09:50:02 -0300

    Create revertString-01.py

---
+++ python-code/revertString-01.py
@@ -0,0 +1,3 @@
+texto = 'Hello, World! OpenAI is amazing.'; #declarando a String
+inverter = ''.join(reversed(texto.split())) #invertendo o texto, mas mantendo a palavra
+print(inverter) #printando 
