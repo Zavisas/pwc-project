a8800af83122ba69347a27bc9d0f5215342042ae
Joao Zavisas <joaoterassi01@gmail.com>
2023-07-09 20:39:44 -0300

    codigos atualizados

--- CodigosReaalizados-Python/Exercicio-03.py
+++ CodigosReaalizados-Python/Exercicio-03.py
@@ -10,12 +10,12 @@ def longestPalindrome(s):
 
     longest = ""
     for i in range(len(s)):
-        # Expand around a single character
+    
         substr1 = expandAroundCenter(s, i, i)
         if len(substr1) > len(longest):
             longest = substr1
 
-        # Expand around two characters
+       
         substr2 = expandAroundCenter(s, i, i + 1)
         if len(substr2) > len(longest):
             longest = substr2
