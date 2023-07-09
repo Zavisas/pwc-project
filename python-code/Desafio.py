6ea32cd3d628a1ec4fa9fb31c0df016c6d780cba
Joao Zavisas <joaoterassi01@gmail.com>
2023-07-09 10:27:27 -0300

    Update Desafio.py

--- python-code/Desafio.py
+++ python-code/Desafio.py
@@ -1,2 +1,11 @@
-print("|========= Bem-vindo =========|")
-print("Qual ação voce deseja realizar?")
+Desafios/
+    ├── Exercicio1/
+    │   └── exercicio.py
+    ├── Exercicio2/
+    │   └── exercicio2.py
+    ├── Exercicio3/
+    │   └── exercicio3.py
+    ├── Exercicio4/
+    │   └── exercicio4.py
+    └── Exercicio5/
+        └── exercicio5.py
